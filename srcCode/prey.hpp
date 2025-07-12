eee515a65217d12b6e2a0d4712d4a3a4f36d32f5
milacasali <76772629+milacasali@users.noreply.github.com>
2025-07-12 11:48:37 +0200

    Update prey.hpp

--- srcCode/prey.hpp
+++ srcCode/prey.hpp
@@ -21,10 +21,12 @@ inline Prey::Prey(Vec3 position, Vec3 velocity)
     : Boid(position, velocity) {}
 
 inline void Prey::stall(const Vec3& wind, double max_speed) {
-  if ((velocity() - wind).norm() <= 0.4 * max_speed) {
+  if ((velocity() - wind).norm() <= 0.4 * max_speed) { //il boid è troppo lento rispetto al vento: inizia a cadere
     updateBoidVelocity(
         wind,
-        Vec3((velocity() - wind).x_, (velocity() - wind).y_, 0.4 * max_speed),
+        Vec3((velocity() - wind).x_, (velocity() - wind).y_, 0.4 * max_speed), //velocità che voglio aggiungere: sulla z cade (è positivo verso il basso)
+                                                                               // su x,y il boid accellera rispetto all'aria in basa alla velocità
+                                                                                //a cui si sta già muovedndo raddopoiando la velocità relativa
         max_speed);
   }
 }
