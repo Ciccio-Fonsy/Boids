023da48ba916fe7c15d358e8498598f2f5881c2a
milacasali <76772629+milacasali@users.noreply.github.com>
2025-07-12 10:55:13 +0200

    Update vec3.cpp

--- srcCode/vec3.cpp
+++ srcCode/vec3.cpp
@@ -1,11 +1,11 @@
 #include "vec3.hpp"
 
-#include <cassert>
+//#include <cassert> //non ci va
 #include <cmath>
 
 namespace boids {
 Vec3 Vec3::vecDistance(bool toroidal, const Vec3& other,
-                       const Vec3& width) const { // From b to a
+                       const Vec3& width) const { // From other to this
   if (toroidal) {
     double dx = x_ - other.x_;
     double dy = y_ - other.y_;
@@ -14,7 +14,7 @@ Vec3 Vec3::vecDistance(bool toroidal, const Vec3& other,
     if (std::abs(dx) > width.x_ / 2) { dx += (dx > 0) ? -width.x_ : width.x_; }
     if (std::abs(dy) > width.y_ / 2) { dy += (dy > 0) ? -width.y_ : width.y_; }
 
-    Vec3 dist(dx, dy, dz);
+    Vec3 dist(dx, dy, dz); //poteva essere return vec3(dx,dy,dz);
     return dist;
   }
   return *this - other;
